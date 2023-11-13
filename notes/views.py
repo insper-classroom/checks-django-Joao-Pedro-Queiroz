989ed315603c63db76b753a835ef036438e60e76
João-Pedro-Queiro <jpqv0105@gmail.com>
2023-11-13 10:59:13 -0300

    Check 4

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
+from .models import Note
 
 def index(request):
-    return HttpResponse("<h1>Olá mundo!</h1><p>Este é o app notes de <em>DevLife do Insper</em>.</p>")
+    conteudo = '<h1>Get-it</h1>'
+    all_notes = Note.objects.all().order_by("-created_at")
+
+    for id, note in enumerate(all_notes):
+        if id == 0:
+            conteudo += f'<ul><li>{note.title}</li>'
+        elif id == len(all_notes) - 1:
+            conteudo += f'<li>{note.title}</li></ul>'
+        else:
+            conteudo += f'<li>{note.title}</li>'
+
+    return HttpResponse(conteudo)
 
 
 def resposta_creditos(request):
