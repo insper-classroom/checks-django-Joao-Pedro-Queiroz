6750b6bfd88fc5dde20deb6ca7091f7191adac75
João-Pedro-Queiro <jpqv0105@gmail.com>
2023-11-13 14:20:56 -0300

    Check 6

--- notes/views.py
+++ notes/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Note
 
 def index(request):
-    all_notes = Note.objects.all().order_by("-created_at")
-
-    return render(request, 'notes/index.html', {'notes': all_notes})
+    if request.method == 'POST':
+        nwe_note = Note(title=request.POST.get('titulo'), content=request.POST.get('detalhes'))
+        nwe_note.save()
+        return redirect('index')
+    else:
+        all_notes = Note.objects.all().order_by("-created_at")
+        return render(request, 'notes/index.html', {'notes': all_notes})
 
 def resposta_creditos(request):
      return HttpResponse("<h1>Créditos</h1><p>Sistema web desenvolvido por João Pedro Queiroz Viana</p>")
