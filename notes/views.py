d74b1b2c310f2ecac6aa1c2b40d5062a6786007f
João-Pedro-Queiro <jpqv0105@gmail.com>
2023-11-13 12:56:23 -0300

    Chack 5

--- notes/views.py
+++ notes/views.py
@@ -1,20 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Note
 
 def index(request):
-    conteudo = '<h1>Get-it</h1>'
     all_notes = Note.objects.all().order_by("-created_at")
 
-    for id, note in enumerate(all_notes):
-        if id == 0:
-            conteudo += f'<ul><li>{note.title}</li>'
-        elif id == len(all_notes) - 1:
-            conteudo += f'<li>{note.title}</li></ul>'
-        else:
-            conteudo += f'<li>{note.title}</li>'
-
-    return HttpResponse(conteudo)
-
+    return render(request, 'notes/index.html', {'notes': all_notes})
 
 def resposta_creditos(request):
      return HttpResponse("<h1>Créditos</h1><p>Sistema web desenvolvido por João Pedro Queiroz Viana</p>")
