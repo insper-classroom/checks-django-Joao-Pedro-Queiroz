f9cdd49b254486c3dcf8fb8251b09e6caee28326
João-Pedro-Queiro <jpqv0105@gmail.com>
2023-11-14 10:10:05 -0300

    Check 7

--- notes/urls.py
+++ notes/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('creditos/', views.resposta_creditos),
+    path('anotacoes/<int:note_id>/apagar', views.delete_note, name='delete_note'),
+    path('anotacoes/<int:note_id>', views.edit_note, name='edit_note'),
     path('', views.index, name='index'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -4,12 +4,31 @@ from .models import Note
 
 def index(request):
     if request.method == 'POST':
-        nwe_note = Note(title=request.POST.get('titulo'), content=request.POST.get('detalhes'))
-        nwe_note.save()
+        new_note = Note(title=request.POST.get('titulo'), content=request.POST.get('detalhes'))
+        new_note.save()
         return redirect('index')
     else:
         all_notes = Note.objects.all().order_by("-created_at")
         return render(request, 'notes/index.html', {'notes': all_notes})
+    
+
+def delete_note(request, note_id):
+    note = Note.objects.get(id=note_id)
+    note.delete()
+    return redirect('index')
+
+
+def edit_note(request, note_id): 
+    if request.method == 'POST':
+        note = Note.objects.get(id=note_id)
+        note.title = request.POST.get('titulo')
+        note.content = request.POST.get('detalhes')
+        note.save()
+        return redirect('index')
+    else:
+        note = Note.objects.get(id=note_id)
+        return render(request, 'notes/edit.html', {'note': note})
+
 
 def resposta_creditos(request):
      return HttpResponse("<h1>Créditos</h1><p>Sistema web desenvolvido por João Pedro Queiroz Viana</p>")
